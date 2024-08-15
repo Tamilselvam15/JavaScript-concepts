bfa2126282588ee121da108120a160b8ba7f63c9
Tamilselvam15 <tamil2002selvam@gmail.com>
2024-08-15 11:22:34 +0530

    Functions concept completed

--- sample.js
+++
@@ -1,9 +0,0 @@
-// Prompt the user to enter their name
-const userName = prompt("Enter Your Name:");
-
-// Check if the user provided a name or canceled the prompt
-if (userName !== null) {
-    console.log(`Hello, ${userName}!`);
-} else {
-    console.log("No name was entered.");
-}
